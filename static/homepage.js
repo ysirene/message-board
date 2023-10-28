d06f48ea07390eb465948f27be3bb62facf5186e
yashan <leeys2423@gmail.com>
2023-10-28 17:34:54 +0800

    Render historical comments based on DB data

--- static/homepage.js
+++ static/homepage.js
@@ -10,9 +10,7 @@ function ajax(src, options){
     });
 };
 
-// TODO: function clearFormData(){
-
-function newMessage(event){
+function addMessage(event){
     event.preventDefault();
     let newMessageFormData = new FormData(document.querySelector('#newMessage'));
     let src = '/api/message';
@@ -21,6 +19,34 @@ function newMessage(event){
         body: newMessageFormData
     };
     ajax(src, options).then((data) => {
-        console.log(data);
+        if(data.ok){
+            location.reload()
+        }else{
+            console.log(data)
+        };
     });
 };
+
+(function renderMessage(){
+    let src = '/api/message';
+    let options = {
+        method: 'GET',
+    };
+    ajax(src, options).then((data) => {
+        console.log(data)
+        let messageBoardElem = document.querySelector('#message_board');
+        for(let i=0; i<data.length; i++){
+            let sectionTag = document.createElement('section');
+            let messageDiv = document.createElement('div');
+            let img = document.createElement('img');
+            let divider = document.createElement('hr');
+            messageDiv.textContent = data[i]['comment'];
+            img.setAttribute('src', data[i]['image_url']);
+            sectionTag.appendChild(messageDiv);
+            sectionTag.appendChild(img);
+            messageBoardElem.appendChild(divider);
+            messageBoardElem.appendChild(sectionTag);
+        }
+    });
+
+})();
