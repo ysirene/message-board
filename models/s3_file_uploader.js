c1f24db76d636d3d5a9273b47159e35b01f2e108
yashan <leeys2423@gmail.com>
2023-10-28 19:20:48 +0800

    Refactor S3-connector.js for Promise-based file upload.

--- models/s3_file_uploader.js
+++ models/s3_file_uploader.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const AWS = require('aws-sdk');
+const { json } = require('express');
 
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -8,20 +9,22 @@ const s3 = new AWS.S3({
 });
 
 function uploadFileToS3(file, fileName, ext){
-    const params = {
-        Bucket: process.env.S3_BUCKET,
-        Key: fileName, // 儲存在 S3 上的檔案名稱
-        Body: file, // 檔案
-        ContentType: ext // 副檔名
-    };
-    s3.upload(params, function(err, data) {
-        if(err){
-            errorMessage = err + err.stack;
-            return {'error': true, 'message': errorMessage};
-        }else{
-            return {'ok': true};
+    return new Promise((resolve, reject) => {
+        const params = {
+            Bucket: process.env.S3_BUCKET,
+            Key: fileName, // 儲存在 S3 上的檔案名稱
+            Body: file, // 檔案
+            ContentType: ext // 副檔名
         };
-    });
+        s3.upload(params, (err, data) => {
+            if(err){
+                errorMessage = err + err.stack;
+                callback({'error': true, 'message': errorMessage});
+            }else{
+                callback({'ok': true});
+            };
+        });
+    })
 };
 
 exports.uploadFileToS3 = uploadFileToS3;
